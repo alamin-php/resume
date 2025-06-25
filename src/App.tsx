a20dd1e92868936b88c5887a44fee1751711ee96
alamin-php <alamin.php100@gmail.com>
2025-06-25 12:31:16 +0600

    not found removed

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,6 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/contact" element={<Contact />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
