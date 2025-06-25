139c2a8408accab7623b8fbe6e09e157ede116be
alamin-php <alamin.php100@gmail.com>
2025-06-25 12:55:50 +0600

    base name update

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/resume">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
